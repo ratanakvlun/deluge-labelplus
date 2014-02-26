7e796dc83c619713db8874b50eb7fc7d99780cbc
Ratanak Lun <ratanakvlun@gmail.com>
2014-02-26 13:54:45 -0500

    gtkui: Rewrite label selection menu

--- labelplus/gtkui/label_selection_menu.py
+++ labelplus/gtkui/label_selection_menu.py
@@ -2,114 +2,117 @@
 # label_selection_menu.py
 #
 # Copyright (C) 2014 Ratanak Lun <ratanakvlun@gmail.com>
-# Copyright (C) 2008 Martijn Voncken <mvoncken@gmail.com>
 #
-# Deluge is free software.
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# You may redistribute it and/or modify it under the terms of the
-# GNU General Public License, as published by the Free Software
-# Foundation; either version 3 of the License, or (at your option)
-# any later version.
-#
-# deluge is distributed in the hope that it will be useful,
+# This software is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with deluge.    If not, write to:
-# 	The Free Software Foundation, Inc.,
-# 	51 Franklin Street, Fifth Floor
-# 	Boston, MA  02110-1301, USA.
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-#    In addition, as a special exception, the copyright holders give
-#    permission to link the code of portions of this program with the OpenSSL
-#    library.
-#    You must obey the GNU General Public License in all respects for all of
-#    the code used other than OpenSSL. If you modify file(s) with this
-#    exception, you may extend this exception to your version of the file(s),
-#    but you are not obligated to do so. If you do not wish to do so, delete
-#    this exception statement from your version. If you delete this exception
-#    statement from all source files in the program, then also delete it here.
+# Linking this software with other modules is making a combined work
+# based on this software. Thus, the terms and conditions of the GNU
+# General Public License cover the whole combination.
+#
+# As a special exception, the copyright holders of this software give
+# you permission to link this software with independent modules to
+# produce a combined work, regardless of the license terms of these
+# independent modules, and to copy and distribute the resulting work
+# under terms of your choice, provided that you also meet, for each
+# linked module in the combined work, the terms and conditions of the
+# license of that module. An independent module is a module which is
+# not derived from or based on this software. If you modify this
+# software, you may extend this exception to your version of the
+# software, but you are not obligated to do so. If you do not wish to
+# do so, delete this exception statement from your version.
 #
 
+
+import datetime
 
 import gtk
 
-from deluge import component
+import labelplus.common
+import labelplus.common.label
 
-from labelplus.common.constant import PLUGIN_NAME
-
-import labelplus.common.label as Label
-
-
-MENU_ITEM = 0
-HANDLER = 1
+import labelplus.gtkui.util
 
 
 class LabelSelectionMenu(gtk.MenuItem):
 
+  def __init__(self, label, plugin, select_func, base_items=[]):
 
-  def __init__(self, label, on_select_label, base_items=[]):
+    super(LabelSelectionMenu, self).__init__(label)
 
-    gtk.MenuItem.__init__(self, label)
+    self._plugin = plugin
+    self._select_func = select_func
+    self._base_items = base_items
 
-    self.plugin = component.get("GtkPlugin.%s" % PLUGIN_NAME)
+    self._last_updated = labelplus.common.DATETIME_010101
 
-    self.on_select_label = on_select_label
-    self.base_items = base_items
-
-    self.submenu = gtk.Menu()
-    self.set_submenu(self.submenu)
-
-    self.connect("activate", self.on_activate)
+    self.set_submenu(gtk.Menu())
+    self.connect("activate", self._on_activate)
 
 
-  def _build_menu(self):
+  def _on_activate(self, widget):
 
-    for child in self.submenu.get_children():
-      self.submenu.remove(child)
+    if self._last_updated <= self._plugin.last_updated:
+      self._clear_menu()
+      self._build_menu(self._plugin.store)
+      self.show_all()
 
-      if child not in self.base_items:
-        child.destroy()
-
-    for item in self.base_items:
-      self.submenu.append(item)
-
-    labels = self.plugin.get_labels()
-    self._load_labels(labels)
+      self._last_updated = datetime.datetime.now()
 
 
-  def _load_labels(self, labels):
+  def _clear_menu(self):
 
-    if labels:
-      labels = sorted(labels, key=lambda x: (Label.get_parent(x[0]), x[1]))
-
-    items = {}
-    for id, name in labels:
-      item = gtk.MenuItem(name)
-      handler = item.connect("activate", self.on_select_label, id)
-      items[id] = (item, handler)
-
-      parent_id = Label.get_parent(id)
-      parent = items.get(parent_id)
-      if parent:
-        if parent[MENU_ITEM].get_submenu() is None:
-          parent[MENU_ITEM].handler_disconnect(parent[HANDLER])
-          parent[MENU_ITEM].set_submenu(gtk.Menu())
-
-          # Create subitem for selecting items with submenus
-          parent_item = gtk.MenuItem(parent[MENU_ITEM].get_label())
-          parent_item.connect("activate", self.on_select_label, parent_id)
-          parent[MENU_ITEM].get_submenu().append(parent_item)
-          parent[MENU_ITEM].get_submenu().append(gtk.SeparatorMenuItem())
-
-        parent[MENU_ITEM].get_submenu().append(item)
-      else:
-        self.submenu.append(item)
+    for item in list(self.get_submenu().get_children()):
+      self.get_submenu().remove(item)
 
 
-  def on_activate(self, widget):
+  def _build_menu(self, model):
 
-    self._build_menu()
-    self.show_all()
+    def create_item(model, row, menus):
+
+      if row:
+        id, data = model[row]
+
+        if id in labelplus.common.label.RESERVED_IDS:
+          return
+
+        name = data["name"]
+
+        item = gtk.MenuItem(name)
+        menus[-1].append(item)
+
+        if not model.iter_has_child(row):
+          item.connect("activate", self._select_func, id)
+        else:
+          sub_item = gtk.MenuItem(name)
+          sub_item.connect("activate", self._select_func, id)
+
+          item.set_submenu(gtk.Menu())
+          item.get_submenu().append(sub_item)
+          item.get_submenu().append(gtk.SeparatorMenuItem())
+
+          # Push the menu to be appended by children
+          menus.append(item.get_submenu())
+
+
+    def pop_menu(model, row, menus):
+
+      if row and model.iter_has_child(row):
+        menus.pop()
+
+
+    for item in self._base_items:
+      self.get_submenu().append(item)
+
+    labelplus.gtkui.util.treemodel_recurse(model, None, create_item,
+      pop_menu, menus=[self.get_submenu()])
