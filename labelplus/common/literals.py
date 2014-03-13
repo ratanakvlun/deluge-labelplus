dff285700b0ce60d86d75be4469f4a5cdba9feb5
Ratanak Lun <ratanakvlun@gmail.com>
2014-03-13 01:22:51 -0400

    common: Add a literals module for defining common strings

---
+++ labelplus/common/literals.py
@@ -0,0 +1,58 @@
+#
+# literals.py
+#
+# Copyright (C) 2014 Ratanak Lun <ratanakvlun@gmail.com>
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Linking this software with other modules is making a combined work
+# based on this software. Thus, the terms and conditions of the GNU
+# General Public License cover the whole combination.
+#
+# As a special exception, the copyright holders of this software give
+# you permission to link this software with independent modules to
+# produce a combined work, regardless of the license terms of these
+# independent modules, and to copy and distribute the resulting work
+# under terms of your choice, provided that you also meet, for each
+# linked module in the combined work, the terms and conditions of the
+# license of that module. An independent module is a module which is
+# not derived from or based on this software. If you modify this
+# software, you may extend this exception to your version of the
+# software, but you are not obligated to do so. If you do not wish to
+# do so, delete this exception statement from your version.
+#
+
+
+ERR_STR = "Error"
+
+ERR_CORE_NOT_INITIALIZED = "Plugin core not initialized"
+ERR_UI_NOT_INITIALIZED = "Plugin UI not initialized"
+ERR_TIMED_OUT = "Timed out"
+
+ERR_INVALID_CHARS = "Invalid characters"
+ERR_INVALID_TYPE = "Invalid type"
+
+ERR_EMPTY_LABEL = "Empty label"
+ERR_INVALID_LABEL = "Invalid label"
+ERR_INVALID_LABEL_ID = "Invalid label id"
+ERR_INVALID_PARENT = "Invalid parent"
+ERR_INVALID_PARENT_ID = "Invalid parent id"
+ERR_LABEL_EXISTS = "Label already exists"
+
+
+OP_ADD_LABEL = "Add label"
+OP_RENAME_LABEL = "Rename label"
+
+OP_LOAD_OPTIONS = "Load options"
+OP_SAVE_OPTIONS = "Save options"
