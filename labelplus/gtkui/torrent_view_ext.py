ea872eac0f2ddceff56fb48cadabc49da85f4e07
Ratanak Lun <ratanakvlun@gmail.com>
2014-03-16 21:55:16 -0400

    gtkui/torrent_view_ext: Revise

--- labelplus/gtkui/torrent_view_ext.py
+++ labelplus/gtkui/torrent_view_ext.py
@@ -46,8 +46,25 @@ import labelplus.common.label
 
 from deluge.ui.client import client
 
-from labelplus.gtkui.label_selection_menu import LabelSelectionMenu
 from labelplus.gtkui.label_options_dialog import LabelOptionsDialog
+from labelplus.gtkui.label_selection_menu import LabelSelectionMenu
+
+
+from labelplus.common import (
+  DISPLAY_NAME,
+
+  STATUS_NAME, STATUS_ID,
+)
+
+from labelplus.common.label import (
+  ID_NULL, ID_ALL, ID_NONE, RESERVED_IDS,
+)
+
+from labelplus.common.literals import (
+  TITLE_SET_FILTER, TITLE_SET_LABEL,
+
+  STR_ALL, STR_NONE, STR_PARENT, STR_SELECTED,
+)
 
 
 log = logging.getLogger(__name__)
@@ -61,22 +78,34 @@ class TorrentViewExt(object):
 
   def __init__(self, plugin):
 
-    log.debug("Initializing TorrentViewExt...")
+    self._plugin = plugin
+    self._view = deluge.component.get("TorrentView")
+    self._menubar = deluge.component.get("MenuBar")
+
+    self._store = None
+
+    self._menu = None
+    self._sep = None
+    self._submenus = []
+
+    self._alt_menu = None
+
+    self._handlers = []
 
     try:
-      self._plugin = plugin
-
-      self._view = deluge.component.get("TorrentView")
-      self._handlers = []
+      self._store = plugin.store.copy()
+      RT.register(self._store, __name__)
 
       self._add_column()
-      self._add_context_menus()
+
+      self._create_menus()
+      self._install_context_menu()
 
       self._register_handlers()
 
-      log.debug("TorrentViewExt initialized")
+      self._plugin.register_update_func(self.update_store)
     except:
-      log.debug("Error initializing TorrentViewExt")
+      self.unload()
       raise
 
 
@@ -84,37 +113,42 @@ class TorrentViewExt(object):
 
     def cell_data_func(column, cell, model, row, indices):
 
-      id = model[row][indices[1]]
       name = model[row][indices[0]]
+      id = model[row][indices[1]]
+
+      if id not in RESERVED_IDS and id in self._store:
+        if self._plugin.config["common"]["torrent_view_fullname"]:
+          name = self._store[id]["fullname"]
+        else:
+          name = self._store[id]["name"]
+
       cell.set_property("text", name)
 
 
-    log.debug("Adding column...")
+    self._view.add_func_column(DISPLAY_NAME, cell_data_func,
+      col_types=[str, str], status_field=[STATUS_NAME, STATUS_ID])
 
-    self._view.add_func_column(labelplus.common.DISPLAY_NAME,
-      cell_data_func, col_types=[str, str],
-      status_field=[labelplus.common.STATUS_NAME, labelplus.common.STATUS_ID])
+    RT.register(self._get_view_column(), __name__)
 
 
-  def _add_context_menus(self):
+  def _create_menus(self):
 
-    log.debug("Adding context menus...")
-
-    menubar = deluge.component.get("MenuBar")
-
-    self._sep = menubar.add_torrentmenu_separator()
-    self._menu = self._create_torrent_menu()
-    menubar.torrentmenu.append(self._menu)
-
-    RT.register(self._sep, "context separator")
-    RT.register(self._menu, "context menu")
+    self._menu = self._create_context_menu()
+    self._submenus = self._create_submenus()
+    self._install_submenus()
 
     self._alt_menu = self._create_alternate_menu()
 
 
-  def _register_handlers(self):
+  def _install_context_menu(self):
 
-    log.debug("Registering handlers...")
+    self._sep = self._menubar.add_torrentmenu_separator()
+    self._menubar.torrentmenu.append(self._menu)
+
+    RT.register(self._sep, __name__)
+
+
+  def _register_handlers(self):
 
     self._register_handler(self._view.treeview, "button-press-event",
       self._on_view_button_press)
@@ -124,53 +158,48 @@ class TorrentViewExt(object):
 
   def unload(self):
 
-    log.debug("Deinitializing TorrentViewExt...")
+    self._plugin.deregister_update_func(self.update_store)
 
-    try:
-      self._deregister_handlers()
+    self._deregister_handlers()
 
-      self._remove_context_menus()
-      self._remove_column()
+    self._uninstall_context_menu()
+    self._destroy_menus()
 
-      log.debug("TorrentViewExt deinitialized")
-    except:
-      log.debug("Error deinitializing TorrentViewExt")
-      raise
+    self._remove_column()
 
 
   def _deregister_handlers(self):
-
-    log.debug("Deregistering handlers...")
 
     for widget, handle in self._handlers:
       widget.disconnect(handle)
 
 
-  def _remove_context_menus(self):
+  def _uninstall_context_menu(self):
 
-    log.debug("Removing context menus...")
+    if self._menu in self._menubar.torrentmenu:
+      self._menubar.torrentmenu.remove(self._menu)
 
-    menubar = deluge.component.get("MenuBar")
+    if self._sep in self._menubar.torrentmenu:
+      self._menubar.torrentmenu.remove(self._sep)
 
-    menubar.torrentmenu.remove(self._menu)
-    menubar.torrentmenu.remove(self._sep)
+    self._sep = None
 
-    self._menu.destroy()
 
-    del self._menu
-    del self._sep
-    del self._alt_menu
+  def _destroy_menus(self):
+
+    self._destroy_alternate_menu()
+
+    self._submenus = []
+    self._destroy_context_menu()
 
 
   def _remove_column(self):
 
-    log.debug("Removing column...")
-
-    # Workaround for Deluge 1.3.6 bug
-    column = self._view.columns[labelplus.common.DISPLAY_NAME]
-    column.column_indices = sorted(column.column_indices, reverse=True)
-
-    self._view.remove_column(labelplus.common.DISPLAY_NAME)
+    # Workaround for Deluge removing indices in the wrong order
+    column = self._view.columns.get(DISPLAY_NAME)
+    if column:
+      column.column_indices = sorted(column.column_indices, reverse=True)
+      self._view.remove_column(DISPLAY_NAME)
 
 
   # Section: General
@@ -181,23 +210,30 @@ class TorrentViewExt(object):
     self._handlers.append((obj, handle))
 
 
+  def _get_view_column(self):
+
+    column = self._view.columns.get(DISPLAY_NAME)
+    if column:
+      return column.column
+
+    return None
+
+
   def _get_selected_torrents_label(self):
 
-    torrent_ids = self._view.get_selected_torrents()
     label_id = None
+    torrent_ids = self._view.get_selected_torrents()
 
-    if len(torrent_ids) > 0:
+    if torrent_ids:
       status = self._view.get_torrent_status(torrent_ids[0])
-      if labelplus.common.STATUS_ID not in status:
+      if STATUS_ID not in status:
         return None
 
-      label_id = status[labelplus.common.STATUS_ID] or \
-        labelplus.common.label.ID_NONE
+      label_id = status[STATUS_ID] or ID_NONE
 
       for id in torrent_ids:
         status = self._view.get_torrent_status(id)
-        other_label_id = status[labelplus.common.STATUS_ID] or \
-          labelplus.common.label.ID_NONE
+        other_label_id = status[STATUS_ID] or ID_NONE
 
         if other_label_id != label_id:
           return None
@@ -205,12 +241,265 @@ class TorrentViewExt(object):
     return label_id
 
 
-  def _get_view_column(self):
+  def _get_any_selected_label(self):
 
-    return self._view.columns[labelplus.common.DISPLAY_NAME].column
+    id = self._get_selected_torrents_label()
+    if id:
+      return id
+    else:
+      sidebar = self._plugin.get_extension("SidebarExt")
+      if sidebar and sidebar.page_selected():
+        return sidebar.get_selected_label()
+
+    return None
 
 
-  # Section: Torrent View: Handlers
+  def _set_filter(self, id):
+
+    if id in self._store:
+      sidebar = self._plugin.get_extension("SidebarExt")
+      if sidebar:
+        sidebar.select_label(id)
+      else:
+        if id == ID_ALL:
+          filter = {}
+        else:
+          filter = {STATUS_ID: id}
+
+        log.info("Setting filter to %r", id if id in RESERVED_IDS else
+          self._store[id]["fullname"])
+        self._view.set_filter(filter)
+
+
+  def _is_filter(self, id):
+
+    ids = self._view.filter.get(STATUS_ID)
+
+    if ids:
+      if isinstance(ids, (list, tuple)):
+        if len(ids) == 1 and ids[0] == id:
+          return True
+      else:
+        if ids == id:
+          return True
+
+    return False
+
+
+  def _has_valid_parent(self, id):
+
+    if id and id not in RESERVED_IDS:
+      parent_id = labelplus.common.label.get_parent_id(id)
+      if parent_id in self._store:
+        return True
+
+    return False
+
+
+  # Section: Update
+
+  def update_store(self, store):
+
+    self._store.destroy()
+    self._store = store.copy()
+    RT.register(self._store, __name__)
+
+    self._destroy_alternate_menu()
+    self._alt_menu = self._create_alternate_menu()
+
+    self._uninstall_submenus()
+    self._destroy_submenus()
+    self._submenus = self._create_submenus()
+    self._install_submenus()
+
+
+  # Section: Context Menu
+
+  def _create_context_menu(self):
+
+    item = gtk.MenuItem(DISPLAY_NAME)
+    item.set_submenu(gtk.Menu())
+
+    RT.register(item, __name__)
+    RT.register(item.get_submenu(), __name__)
+
+    return item
+
+
+  def _destroy_context_menu(self):
+
+    if self._menu:
+      self._menu.destroy()
+      self._menu = None
+
+
+  def _create_alternate_menu(self):
+
+    item = self._create_context_menu()
+    item.get_submenu().append(self._create_filter_menu())
+
+    menu = gtk.Menu()
+    menu.append(item)
+    menu.show_all()
+
+    RT.register(menu, __name__)
+
+    return menu
+
+
+  def _destroy_alternate_menu(self):
+
+    if self._alt_menu:
+      self._alt_menu.destroy()
+      self._alt_menu = None
+
+
+  # Section: Context Menu: Submenu
+
+  def _create_submenus(self):
+
+    menus = []
+    menus.append(self._create_filter_menu())
+    menus.append(self._create_set_label_menu())
+
+    return menus
+
+
+  def _destroy_submenus(self):
+
+    while self._submenus:
+      menu = self._submenus.pop()
+      menu.destroy()
+
+
+  def _install_submenus(self):
+
+    submenu = self._menu.get_submenu()
+
+    for menu in self._submenus:
+      submenu.append(menu)
+
+    self._menu.show_all()
+
+
+  def _uninstall_submenus(self):
+
+    submenu = self._menu.get_submenu()
+
+    for menu in self._submenus:
+      if menu in submenu:
+        submenu.remove(menu)
+
+
+  # Section: Context Menu: Submenu: Set Filter
+
+  def _create_filter_menu(self):
+
+    def on_activate(widget, label_id):
+
+      self._set_filter(label_id)
+
+
+    def on_activate_parent(widget):
+
+      id = self._get_any_selected_label()
+      parent_id = labelplus.common.label.get_parent_id(id)
+      on_activate(widget, parent_id)
+
+
+    def on_activate_selected(widget):
+
+      id = self._get_selected_torrents_label()
+      on_activate(widget, id)
+
+
+    def on_show_menu(widget):
+
+      items[0].hide()
+      items[1].hide()
+
+      id = self._get_any_selected_label()
+      if self._has_valid_parent(id):
+        items[0].show()
+
+      id = self._get_selected_torrents_label()
+      if id and id not in RESERVED_IDS:
+        items[1].show()
+
+
+    root_items = (
+      ((gtk.MenuItem, _(STR_ALL)), on_activate, ID_ALL),
+      ((gtk.MenuItem, _(STR_NONE)), on_activate, ID_NONE),
+    )
+
+    menu = LabelSelectionMenu(self._store.model, on_activate,
+      root_items=root_items)
+    menu.connect("show", on_show_menu)
+
+    items = labelplus.gtkui.common.menu_add_items(menu, 2,
+      (
+        ((gtk.MenuItem, _(STR_PARENT)), on_activate_parent),
+        ((gtk.MenuItem, _(STR_SELECTED)), on_activate_selected),
+      )
+    )
+
+    root = gtk.MenuItem(_(TITLE_SET_FILTER))
+    root.set_submenu(menu)
+
+    RT.register(menu, __name__)
+    RT.register(root, __name__)
+
+    return root
+
+
+  # Section: Context Menu: Submenu: Set Label
+
+  def _create_set_label_menu(self):
+
+    def on_activate(widget, label_id):
+
+      torrent_ids = self._view.get_selected_torrents()
+      if torrent_ids and label_id in self._store:
+        log.info("Setting label for selected torrents to %r", label_id if
+          label_id in RESERVED_IDS else self._store[label_id]["fullname"])
+        client.labelplus.set_torrent_labels(torrent_ids, label_id)
+
+
+    def on_activate_parent(widget):
+
+      id = self._get_selected_torrents_label()
+      parent_id = labelplus.common.label.get_parent_id(id)
+      on_activate(widget, parent_id)
+
+
+    def on_show_menu(widget):
+
+      items[0].hide()
+
+      id = self._get_selected_torrents_label()
+      if self._has_valid_parent(id):
+        items[0].show()
+
+
+    root_items = (((gtk.MenuItem, _(STR_NONE)), on_activate, ID_NONE),)
+
+    menu = LabelSelectionMenu(self._store.model, on_activate,
+      root_items=root_items)
+    menu.connect("show", on_show_menu)
+
+    items = labelplus.gtkui.common.menu_add_items(menu, 1,
+      (((gtk.MenuItem, _(STR_PARENT)), on_activate_parent),))
+
+    root = gtk.MenuItem(_(TITLE_SET_LABEL))
+    root.set_submenu(menu)
+
+    RT.register(menu, __name__)
+    RT.register(root, __name__)
+
+    return root
+
+
+  # Section: Deluge Handlers
 
   def _on_view_button_press(self, widget, event):
 
@@ -219,230 +508,18 @@ class TorrentViewExt(object):
     if not path_info:
       if event.button == 3:
         self._alt_menu.popup(None, None, None, event.button, event.time)
-
       return
 
     if event.button == 1 and event.type == gtk.gdk._2BUTTON_PRESS:
       if path_info[1] == self._get_view_column():
         id = self._get_selected_torrents_label()
-        log.debug("doubleclick on %r", id)
-        log.debug("filter %r", self._view.filter)
-        return
-        sidebar = self._plugin.get_extension("SidebarExt")
-        if sidebar:
-          if sidebar.page_selected() and sidebar.get_selected_label() == id:
-            log.debug("Open label options")
-            #self._do_open_label_options(widget, event)
+
+        if self._is_filter(id):
+          try:
+            dialog = LabelOptionsDialog(self._plugin, id)
+            dialog.show()
+            RT.register(dialog, __name__)
+          except:
+            pass
         else:
-          log.debug("Set label filter")
-          #self._do_go_to_label(widget)
-
-
-  # Section: Torrent View: Context Menu
-
-  def _create_torrent_menu(self):
-
-    def on_open_menu(widget):
-
-      id = self._get_selected_torrents_label()
-      if (id != labelplus.common.label.ID_NONE and
-          id in self._plugin.data):
-        label_options_item.show()
-      else:
-        label_options_item.hide()
-
-
-    jump_menu = self._create_jump_menu()
-    set_label_menu = self._create_set_label_menu()
-    label_options_item = self._create_label_options_item()
-
-    menu = gtk.MenuItem(labelplus.common.DISPLAY_NAME)
-    menu.connect("activate", on_open_menu)
-
-    menu.set_submenu(gtk.Menu())
-    menu.get_submenu().append(jump_menu)
-    menu.get_submenu().append(set_label_menu)
-    menu.get_submenu().append(label_options_item)
-
-    menu.show_all()
-
-    RT.register(jump_menu, "jump menu")
-    RT.register(set_label_menu, "set menu")
-    RT.register(label_options_item, "optionsitem")
-    RT.register(menu, "context menu raw")
-    RT.register(menu.get_submenu(), "context menu submenu")
-
-    return menu
-
-
-  def _create_alternate_menu(self):
-
-    item = gtk.MenuItem(labelplus.common.DISPLAY_NAME)
-    item.set_submenu(gtk.Menu())
-    item.get_submenu().append(self._create_jump_menu())
-
-    menu = gtk.Menu()
-    menu.append(item)
-
-    menu.show_all()
-
-    RT.register(item, "alt item")
-    RT.register(item.get_submenu(), "alt item submenu")
-    RT.register(menu, "alt menu")
-
-    return menu
-
-
-  # Section: Torrent View: Context Menu: Jump
-
-  def _create_jump_menu(self):
-
-    def on_select_label(widget, label_id):
-
-      self._do_go_to_label(widget, label_id)
-
-
-    def on_event(widget):
-
-      log.debug("ON SHOW")
-
-
-    def on_open_menu(widget):
-
-      log.debug("ON OPEN MENU")
-      selected_item.hide()
-      parent_item.hide()
-
-      id = self._get_selected_torrents_label()
-      if id:
-        if getattr(selected_item, "handle", None):
-          selected_item.disconnect(selected_item.handle)
-
-        handle = selected_item.connect("activate", on_select_label, id)
-        selected_item.handle = handle
-        selected_item.show()
-      else:
-        sidebar = self._plugin.get_extension("SidebarExt")
-        if sidebar:
-          if sidebar.page_selected():
-            id = sidebar.get_selected_label()
-
-      if id:
-        parent_id = labelplus.common.label.get_parent_id(id)
-        if parent_id in self._plugin.data:
-          if getattr(parent_item, "handle", None):
-            parent_item.disconnect(parent_item.handle)
-
-          handle = parent_item.connect("activate", on_select_label, parent_id)
-          parent_item.handle = handle
-          parent_item.show()
-
-
-    all_item = gtk.MenuItem(_(labelplus.common.label.ID_ALL))
-    all_item.connect("activate", on_select_label,
-      labelplus.common.label.ID_ALL)
-
-    none_item = gtk.MenuItem(_(labelplus.common.label.ID_NONE))
-    none_item.connect("activate", on_select_label,
-      labelplus.common.label.ID_NONE)
-
-    selected_item = gtk.MenuItem(_("Selected"))
-    parent_item = gtk.MenuItem(_("Parent"))
-    sep = gtk.SeparatorMenuItem()
-
-    items = [all_item, none_item, selected_item, parent_item, sep]
-
-    menu = LabelSelectionMenu(_("Jump To"), self._plugin,
-      on_select_label, items)
-    menu.connect("activate", on_open_menu)
-    menu.get_submenu().connect("show", on_event)
-
-    RT.register(all_item, "jump all")
-    RT.register(none_item, "jump none")
-    RT.register(selected_item, "jump selected")
-    RT.register(parent_item, "jump parent")
-    RT.register(sep, "jump sep")
-    RT.register(menu, "jump menu")
-    RT.register(menu.get_submenu(), "jump submenu")
-    return menu
-
-
-  def _do_go_to_label(self, widget, id=None):
-
-    if not id:
-      id = self._get_selected_torrents_label()
-
-    log.debug("should set filter to %r", id)
-    return
-    if id is not None:
-      self.label_sidebar.select_label(id)
-
-
-  # Section: Torrent View: Context Menu: Set Label
-
-  def _create_set_label_menu(self):
-
-    def on_select_label(widget, label_id):
-
-      torrent_ids = self._view.get_selected_torrents()
-      if torrent_ids:
-        log.debug("Setting label for selected torrents to %r", label_id)
-        client.labelplus.set_torrent_labels(torrent_ids, label_id)
-
-
-    def on_open_menu(widget):
-
-      parent_item.hide()
-
-      id = self._get_selected_torrents_label()
-      if id:
-        parent_id = labelplus.common.label.get_parent_id(id)
-        if parent_id in self._plugin.data:
-          if getattr(parent_item, "handle", None):
-            parent_item.disconnect(parent_item.handle)
-
-          handle = parent_item.connect("activate", on_select_label, parent_id)
-          parent_item.handle = handle
-          parent_item.show()
-
-
-    none_item = gtk.MenuItem(_(labelplus.common.label.ID_NONE))
-    none_item.connect("activate", on_select_label,
-      labelplus.common.label.ID_NONE)
-
-    parent_item = gtk.MenuItem(_("Parent"))
-    sep = gtk.SeparatorMenuItem()
-
-    items = [none_item, parent_item, sep]
-
-    menu = LabelSelectionMenu(_("Set Label"), self._plugin,
-      on_select_label, items)
-    menu.connect("activate", on_open_menu)
-
-    RT.register(none_item, "set none")
-    RT.register(parent_item, "set parent")
-    RT.register(sep, "set sep")
-    RT.register(menu, "set menu")
-    RT.register(menu.get_submenu(), "set submenu")
-    return menu
-
-
-  # Section: Torrent View: Context Menu: Label Options
-
-  def _create_label_options_item(self):
-
-    def on_activate(widget):
-
-      id = self._get_selected_torrents_label()
-      if (id != labelplus.common.label.ID_NONE and
-          id in self._plugin.data):
-        log.debug("Opening label options dialog for %r", id)
-        LabelOptionsDialog(self._plugin, id)
-
-
-    item = gtk.MenuItem(_("Label Options"))
-    item.connect("activate", on_activate)
-
-    RT.register(item, "label options item")
-
-    return item
+          self._set_filter(id)
